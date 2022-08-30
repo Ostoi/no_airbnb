98f1ad3a6061707ef903fdddf851460998a91e9d
Sydel Chayat <mayoindonela@yahoo.com>
2022-08-30 13:50:37 +0300

    done

--- app/controllers/spaceflights_controller.rb
+++ app/controllers/spaceflights_controller.rb
@@ -1,2 +1,13 @@
 class SpaceflightsController < ApplicationController
+  def index
+    @spaceflights = Spaceflight.all
+  end
+
+  def new
+    @spaceflight = Spaceflight.new
+  end
+
+  def create
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database"
+
+5.times do
+  spaceflight = Spaceflight.create!(
+    name: Faker::Space.nasa_space_craft,
+    # user_id:
+    duration: Faker::Space.nasa_space_craft,
+    overview: Faker::Space.nasa_space_craft,
+    cabin_class: Faker::Space.nasa_space_craft,
+    departure_location: Faker::Space.nasa_space_craft,
+    destination: Faker::Space.nasa_space_craft
+  )
+  spaceflight.save!
+end
+puts "Finished!"
