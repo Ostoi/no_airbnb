be51bd0ab9810144de040b1f15849cc0fc035eca
Melchior Brincken <christoph.brincken@gmx.at>
2022-08-30 12:49:36 +0200

    also done

--- app/controllers/spaceflights_controller.rb
+++ app/controllers/spaceflights_controller.rb
@@ -1,2 +1,5 @@
 class SpaceflightsController < ApplicationController
+  def show
+    @spaceflight = Spaceflight.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(email: "some_email@some.com", password: "123456")
+
+Spaceflight.create(name: "Space X", duration: 80, overview: "Nice view of the Galaxy", user:user)
+puts "finish"
